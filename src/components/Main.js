795e90153af710cc8b5ae645cb7f93373afcf88e
Ariana <74054656+VincitQuiPatitur@users.noreply.github.com>
2022-12-22 19:43:48 +0400

    refactor: API is connected

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,39 @@
 import React from "react";
-import profileAvatar from "../images/avatar.jpg";
+import {api} from "../utils/api.js";
+import avatar from "../images/avatar.jpg"
 
 function Main(props) {
+    const [userName, setUserName] = React.useState('Арина Кострова');
+    const [userDescription, setUserDescription] = React.useState('Программирую и путешествую');
+    const [userAvatar, setUserAvatar] = React.useState(`${avatar}`);
+
+    React.useEffect(() =>{
+        api.getUserInfo()
+            .then((result) => {
+                setUserName(result.name);
+                setUserDescription(result.about);
+                setUserAvatar(result.avatar)
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    });
+
 
     return (
         <main className="main">
         <section className="profile">
             <div className="profile__avatar"
             onClick={props.onEditAvatar}>
-                <img src={profileAvatar} alt="Фото пользователя"
+                <img src={userAvatar} alt="Фото пользователя"
                      className="profile__avatar-image"/>
             </div>
             <div className="profile__info">
-                <h1 className="profile__user-name">Кострова Арина</h1>
+                <h1 className="profile__user-name">{userName}</h1>
                 <button className="profile__edit-button" id='profileEditBtn' type="button"
                         aria-label="Кнопка редактирования профиля"
                 onClick={props.onEditProfile}></button>
-                <p className="profile__description">Программирую и путешествую</p>
+                <p className="profile__description">{userDescription}</p>
             </div>
             <button className="profile__add-button" type="button" aria-label="Добавить пост"
                     onClick={props.onAddPlace}></button>
